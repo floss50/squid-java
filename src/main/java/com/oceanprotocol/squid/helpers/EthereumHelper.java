4b646a096152d521b162ceb0b44bd18b67f8e112
aitor@oceanprotocol.com <aitor@oceanprotocol.com>
2019-01-24 12:51:19 +0100

    adding EthereumHelper UT

--- src/main/java/com/oceanprotocol/squid/helpers/EthereumHelper.java
+++ src/main/java/com/oceanprotocol/squid/helpers/EthereumHelper.java
@@ -4,9 +4,7 @@ import org.web3j.crypto.Credentials;
 import org.web3j.crypto.Hash;
 import org.web3j.crypto.Sign;
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.methods.response.EthSign;
-import org.web3j.utils.Numeric;
 
 import java.io.IOException;
 
@@ -43,30 +41,5 @@ public abstract class EthereumHelper {
                 .substring(0, 10);
     }
 
-    public static byte[] getFunctionSelectorBytes(String functionDefinition)    {
-        return Numeric.hexStringToByteArray(
-                Hash.sha3String(functionDefinition).substring(0, 10)
-        );
-    }
-
-    /**
-     * It returns the size in bits of an Abi type
-     * Equivalent to: web3.py size_of_type
-     * @param type abi type
-     * @return the number of bytes or -1 if doesn't have a fixed number of bytes
-     */
-    public static int sizeOfAbiType(String type) {
-        if (type.contains("string"))
-            return -1;
-        else if (type.contains("byte"))
-            return -1;
-        else if (type.contains("["))
-            return -1;
-        else if (type.equals("bool"))
-            return 8;
-        else if (type.equals("address"))
-            return 160;
-        return Integer.parseInt(type.replaceAll("[^\\d.]", ""));
-    }
 
 }
