cd010c95b8f6b075f909f09a0a85b606f9b39e39
aitor@oceanprotocol.com <aitor@oceanprotocol.com>
2019-01-24 10:43:58 +0100

    adding encoding tests

--- src/test/java/com/oceanprotocol/squid/helpers/CryptoHelperTest.java
+++ src/test/java/com/oceanprotocol/squid/helpers/CryptoHelperTest.java
@@ -15,6 +15,7 @@ public class CryptoHelperTest {
     @Test
     public void soliditySha3() throws Exception {
         assertTrue(CryptoHelper.soliditySha3(1).length == 32);
+        assertTrue(CryptoHelper.soliditySha3(1, 2, 3).length == 32);
     }
 
     @Test
@@ -28,7 +29,9 @@ public class CryptoHelperTest {
     public void toBytes() {
 
         assertEquals(32, CryptoHelper.toBytes("hey".getBytes()).length);
+        assertEquals(32, CryptoHelper.toBytes("00a329c0648769A73afAc7F9381E08FB".getBytes()).length);
         assertEquals(32, CryptoHelper.toBytes(BigInteger.TEN).length);
+        assertEquals(32, CryptoHelper.toBytes(BigInteger.valueOf(-1)).length);
 
         assertEquals(20, CryptoHelper.toBytes(
                 new Address("00a329c0648769A73afAc7F9381E08FB43dBEA72")).length);
