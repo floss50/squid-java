ffcc0e99550185579d2caa8504d4216fd1f1cbf5
josepablo <jose@oceanprotocol.com>
2019-01-24 12:03:48 +0100

    Removing some code

--- src/test/java/com/oceanprotocol/squid/api/OceanApiIT.java
+++ src/test/java/com/oceanprotocol/squid/api/OceanApiIT.java
@@ -63,28 +63,6 @@ public class OceanApiIT {
 
     }
 
-    /*
-    @Test
-    public void buildAPIFromProperties() throws Exception {
-
-        // Default values for KEEPER_URL, KEEPER_GAS_LIMIT, KEEPER_GAS_PRICE, AQUARIUS_URL, SECRETSTORE_URL, CONSUME_BASE_PATH
-        Properties properties = new Properties();
-        properties.put(OceanConfig.MAIN_ACCOUNT_ADDRESS, "0x00bd138abd70e2f00903268f3db08f2d25677c9e");
-        properties.put(OceanConfig.MAIN_ACCOUNT_PASSWORD,"node0");
-        properties.put(OceanConfig.MAIN_ACCOUNT_CREDENTIALS_FILE,"src/test/resources/accounts/parity/00bd138abd70e2f00903268f3db08f2d25677c9e.json.testaccount");
-        properties.put(OceanConfig.DID_REGISTRY_ADDRESS, "0x01daE123504DDf108E0C65a42190516E5c5dfc07");
-        properties.put(OceanConfig.SERVICE_AGREEMENT_ADDRESS, "0x21668cE2116Dbc48AC116F31678CfaaeF911F7aA");
-        properties.put(OceanConfig.PAYMENT_CONDITIONS_ADDRESS, "0x38A531cc85A58adCb01D6a249E33c27CE277a2D1");
-        properties.put(OceanConfig.ACCESS_CONDITIONS_ADDRESS, "0x605FAF898Fc7c2Aa847Ba0D558b5251c0F128Fd7");
-        properties.put(OceanConfig.TOKEN_ADDRESS, "0xxxx");
-        properties.put(OceanConfig.OCEAN_MARKET_ADDRESS, "0xxxxx");
-
-        OceanAPI oceanAPIFromProperties = OceanAPI.getInstance(properties);
-        assertEquals(properties.getProperty("account.main.address"), oceanAPIFromProperties.getMainAccount().address);
-
-    }
-    */
-
     @Test
     public void registerAsset() throws Exception {
 
