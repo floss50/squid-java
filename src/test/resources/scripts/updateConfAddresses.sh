bbe79a0f9c94061adcf86e48d38112d03256afa8
aitor@oceanprotocol.com <aitor@oceanprotocol.com>
2019-02-18 11:41:59 +0100

    adapting the SecretStore IT and flagging with @Ignore

--- src/test/resources/scripts/updateConfAddresses.sh
+++ src/test/resources/scripts/updateConfAddresses.sh
@@ -5,12 +5,12 @@ SERVICEAGREEMENT_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artif
 PAYMENTCONDITIONS_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/PaymentConditions.spree.json")
 ACCESSCONDITIONS_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/AccessConditions.spree.json")
 OCEANTOKEN_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/OceanToken.spree.json")
-OCEANMARKET_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/OceanMarket.spree.json")
+DISPENSER_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/Dispenser.spree.json")
 
 sed -i "s/contract.didRegistry.address=.*/contract.didRegistry.address=\"$DIDREGISTRY_ADDRESS\"/g" src/test/resources/application.conf
 sed -i "s/contract.serviceExecutionAgreement.address=.*/contract.serviceExecutionAgreement.address=\"$SERVICEAGREEMENT_ADDRESS\"/g" src/test/resources/application.conf
 sed -i "s/contract.paymentConditions.address=.*/contract.paymentConditions.address=\"$PAYMENTCONDITIONS_ADDRESS\"/g" src/test/resources/application.conf
 sed -i "s/contract.accessConditions.address=.*/contract.accessConditions.address=\"$ACCESSCONDITIONS_ADDRESS\"/g" src/test/resources/application.conf
 sed -i "s/contract.token.address=.*/contract.token.address=\"$OCEANTOKEN_ADDRESS\"/g" src/test/resources/application.conf
-sed -i "s/contract.oceanmarket.address=.*/contract.oceanmarket.address=\"$OCEANMARKET_ADDRESS\"/g" src/test/resources/application.conf
+sed -i "s/contract.dispenser.address=.*/contract.dispenser.address=\"$DISPENSER_ADDRESS\"/g" src/test/resources/application.conf
 
