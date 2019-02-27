e3f0767f9c1263c4266e8b2f51a483eb0227e399
aitor@oceanprotocol.com <aitor@oceanprotocol.com>
2019-02-27 11:30:05 +0100

    updating script and config for loading new contracts

--- src/test/resources/scripts/updateConfAddresses.sh
+++ src/test/resources/scripts/updateConfAddresses.sh
@@ -1,16 +1,27 @@
-#!/bin/sh -x
+#!/bin/bash
 
-DIDREGISTRY_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/DIDRegistry.spree.json")
-SERVICEAGREEMENT_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/ServiceExecutionAgreement.spree.json")
-PAYMENTCONDITIONS_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/PaymentConditions.spree.json")
-ACCESSCONDITIONS_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/AccessConditions.spree.json")
-OCEANTOKEN_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/OceanToken.spree.json")
-DISPENSER_ADDRESS=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/Dispenser.spree.json")
+network="spree"
 
-sed -i "s/contract.didRegistry.address=.*/contract.didRegistry.address=\"$DIDREGISTRY_ADDRESS\"/g" src/test/resources/application.conf
-sed -i "s/contract.serviceExecutionAgreement.address=.*/contract.serviceExecutionAgreement.address=\"$SERVICEAGREEMENT_ADDRESS\"/g" src/test/resources/application.conf
-sed -i "s/contract.paymentConditions.address=.*/contract.paymentConditions.address=\"$PAYMENTCONDITIONS_ADDRESS\"/g" src/test/resources/application.conf
-sed -i "s/contract.accessConditions.address=.*/contract.accessConditions.address=\"$ACCESSCONDITIONS_ADDRESS\"/g" src/test/resources/application.conf
-sed -i "s/contract.token.address=.*/contract.token.address=\"$OCEANTOKEN_ADDRESS\"/g" src/test/resources/application.conf
-sed -i "s/contract.dispenser.address=.*/contract.dispenser.address=\"$DISPENSER_ADDRESS\"/g" src/test/resources/application.conf
+declare -a contracts=(
+    "SignCondition"
+    "HashLockCondition"
+    "LockRewardCondition"
+    "AccessSecretStoreCondition"
+    "EscrowReward"
+    "EscrowAccessSecretStoreTemplate"
+    "OceanToken"
+    "Dispenser"
+    "DIDRegistry"
+    "ConditionStoreManager"
+    "TemplateStoreManager"
+    "AgreementStoreManager"
+)
+
+for c in "${contracts[@]}"
+do
+   address=$(jq -r .address "${HOME}/.ocean/keeper-contracts/artifacts/$c.$network.json")
+   echo "Setting up $c address to $address"
+   sed -i "s/contract.$c.address=.*/contract.$c.address=\"$address\"/g" src/test/resources/application.conf
+
+done
 
