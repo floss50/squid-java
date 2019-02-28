11458509de8339d8ae1d83321f13a8cada6651e8
aitor@oceanprotocol.com <aitor@oceanprotocol.com>
2019-02-28 12:35:51 +0100

    fixing base manager contracts

--- src/main/java/com/oceanprotocol/squid/manager/BaseManager.java
+++ src/main/java/com/oceanprotocol/squid/manager/BaseManager.java
@@ -43,6 +43,8 @@ public abstract class BaseManager {
     protected DIDRegistry didRegistry;
     protected EscrowAccessSecretStoreTemplate escrowAccessSecretStoreTemplate;
     protected LockRewardCondition lockRewardCondition;
+    protected EscrowReward escrowReward;
+    protected AccessSecretStoreCondition accessSecretStoreCondition;
     protected ContractAddresses contractAddresses  = new ContractAddresses();
     protected Config config= ConfigFactory.load();
 
@@ -309,9 +311,47 @@ public abstract class BaseManager {
      * It sets the LockRewardCondition instance
      * @param lockRewardCondition instance
      */
-    public void setLockRewardCondition(LockRewardCondition lockRewardCondition) {
+    public BaseManager setLockRewardCondition(LockRewardCondition lockRewardCondition) {
         this.lockRewardCondition = lockRewardCondition;
+        return this;
     }
+
+    /**
+     * It gets the EscrowReward stub instance
+     * @return EscrowReward instance
+     */
+    public EscrowReward getEscrowReward() {
+        return escrowReward;
+    }
+
+    /**
+     * It sets the EscrowReward instance
+     * @param escrowReward
+     * @return
+     */
+    public BaseManager setEscrowReward(EscrowReward escrowReward) {
+        this.escrowReward = escrowReward;
+        return this;
+    }
+
+    /**
+     * It gets the AccessSecretStoreCondition stub instance
+     * @return AccessSecretStoreCondition instance
+     */
+    public AccessSecretStoreCondition getAccessSecretStoreCondition() {
+        return accessSecretStoreCondition;
+    }
+
+    /**
+     * It sets the EscrowReward instance
+     * @param accessSecretStoreCondition
+     * @return
+     */
+    public BaseManager setAccessSecretStoreCondition(AccessSecretStoreCondition accessSecretStoreCondition) {
+        this.accessSecretStoreCondition = accessSecretStoreCondition;
+        return this;
+    }
+
 
     public Account getMainAccount() {
         return mainAccount;
