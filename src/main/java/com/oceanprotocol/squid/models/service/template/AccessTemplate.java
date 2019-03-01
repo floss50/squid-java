2dcd90e1ec9306860677ed2d5f55f8d2f294ac21
aitor@oceanprotocol.com <aitor@oceanprotocol.com>
2019-03-01 13:00:36 +0100

    rename predefined values

--- src/main/java/com/oceanprotocol/squid/models/service/template/AccessTemplate.java
+++ src/main/java/com/oceanprotocol/squid/models/service/template/AccessTemplate.java
@@ -16,10 +16,10 @@ import java.util.List;
 public class AccessTemplate extends AbstractModel implements FromJsonToModel {
 
     @JsonProperty
-    public String type= "OceanProtocolServiceAgreementTemplate";
+    public String type= "Access";
 
     @JsonProperty
-    public String id= "0x044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d";
+    public String id= "";
 
     @JsonProperty
     public String name= "dataAssetAccessServiceAgreement";
